bb1b471f12518a481aceac44a104760d20eeec84
sagittaracc <sagittaracc@gmail.com>
2025-01-19 05:09:05 +0300

    refactor

--- components/note/note.tsx
+++ components/note/note.tsx
@@ -4,15 +4,13 @@ import { createNote } from '@/lib/actions';
 import React, { useState } from 'react'
 
 const Note = () => {
-  const [note, setNote] = useState('');
-
   return (
     <form action={createNote}>
       <div>
-        <textarea value={note} onChange={e => setNote(e.target.value)} name="note"/>
+        <textarea name="note"/>
       </div>
       <div>
-        <button disabled={note === ''} type="submit">Сохранить</button>
+        <button type="submit">Сохранить</button>
       </div>
     </form>
   )
