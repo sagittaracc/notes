b995641e8537635a14036ed8e5f0c66bee7600a6
sagittaracc <sagittaracc@gmail.com>
2025-01-26 12:17:08 +0300

    добавил у плэйсхолдера опциональные параметры ширина и высота

--- components/placeholder/placeholder.tsx
+++ components/placeholder/placeholder.tsx
@@ -3,11 +3,13 @@ import styles from './placeholder.module.scss';
 
 type TPlaceholderProps = {
   text: string
+  width?: string | number
+  height?: string | number
 }
 
-const Placeholder: FC<TPlaceholderProps> = ({ text }) => {
+const Placeholder: FC<TPlaceholderProps> = ({ text, width = '100%', height = '100%' }) => {
   return (
-    <div className={styles.placeholder}>
+    <div className={styles.placeholder} style={{width, height}}>
       <span>{text}</span>
     </div>
   )
